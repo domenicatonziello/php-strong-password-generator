23f2fe901d79f6cc5cc4c7dc9b315fcecb371ddc
domenicatonziello <domenica.tonziello10@gmail.com>
2023-02-08 17:30:20 +0100

    milestone 2

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
-// creo array
-$characters = 'a b c d e f g h i l m n o p q r s t u v z w y j k x ! £ $ % & / ( ) = ? ^ 1 2 3 4 5 6 7 8 9 0 A B C D E F G H I L M N O P Q R S T U V Z Y J K X';
-// divido stringa
-$characters = explode(' ', $characters);
-// mischio elementi dell'array
-shuffle($characters);
-
-// creo funzione che genera password
-function getRandomPassword($characters, $length)
-{
-    $password = '';
-    do {
-        // prendo un carattere dall'array
-        $random_character = next($characters);
-        // monto stringa
-        $password .= $random_character;
-    } while (strlen($password) < $length);
-    return $password;
-};
-
-// se esiste una chiave 'length'
-if (!empty($_GET['length'])) {
-    $length = $_GET['length'];
-    // chiamo funzione che genera password
-    getRandomPassword($characters, $length);
-    // dichiaro messaggio da inserire
-    $message = getRandomPassword($characters, $length);
-} else {
-    $message = 'Nessun parametro inserito';
-}
-
-
+include 'partials/functions.php';
 ?>
 
 <!DOCTYPE html>
---
+++ redirect.php
@@ -0,0 +1,21 @@
+<?php
+include __DIR__ . '/partials/functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div class="alert alert-primary" role="alert"> La tua password è: <?= $message ?> </div>
+    </main>
+</body>
+
+</html>
